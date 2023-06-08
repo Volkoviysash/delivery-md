8203d5757eeda74e8bfc5963df7745d94923e881
volkovich <volkoviysash@gmail.com>
2023-06-09 01:42:49 +0300

    feat: Building Header Section.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,5 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
+
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
@@ -22,7 +23,7 @@ const HomeScreen = () => {
       <Text className='text-red-500'></Text>
 
       {/* Header */}
-      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+      <View className='flex-row pb-3 items-center mx-4 space-x-2 px-4'>
         <Image
           source={{ uri: "../assets/logoes/delivery.jpeg" }}
           className='h-7 w-7 bg-gray-300 rounded-full'
@@ -37,6 +38,19 @@ const HomeScreen = () => {
         </View>
 
         <UserIcon size={35} color='#00CCBB' />
+      </View>
+
+      {/* Search */}
+      <View className='flex-row items-center space-x-2 pb-2 mx-4 px-4'>
+        <View className='flex-row space-x-2 flex-1 bg-gray-200 p-3'>
+          <MagnifyingGlassIcon color='gray' size={20} />
+          <TextInput
+            placeholder='Restaurants and cuisines'
+            keyboardType='default'
+          />
+        </View>
+
+        <AdjustmentsHorizontalIcon color='#00CCBB' />
       </View>
     </SafeAreaView>
   );
