b53603471e284e4d959fef6750b75fd069664f0e
volkovich <volkoviysash@gmail.com>
2023-06-11 00:54:46 +0300

    feat: Create menu inside Restaurant Screen.

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -2,7 +2,14 @@ import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { urlFor } from "../sanity";
-import { ArrowLeftIcon } from "react-native-heroicons/outline";
+import {
+  ArrowLeftIcon,
+  ChevronRightIcon,
+  MapPinIcon,
+  StarIcon,
+  QuestionMarkCircleIcon,
+} from "react-native-heroicons/outline";
+import DishRow from "../components/DishRow";
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
@@ -40,6 +47,49 @@ const RestaurantScreen = () => {
           <ArrowLeftIcon size={20} color='#00CCBB' />
         </TouchableOpacity>
       </View>
+      <View className='bg-white'>
+        <View className='px-4 pt-4'>
+          <Text className='text-3xl font-bold'>{title}</Text>
+          <View className='flex-row space-x-2 my-1'>
+            <View className='flex-row items-center space-x-1'>
+              <StarIcon color='green' opacity={0.5} size={22} />
+              <Text className='text-xs text-gray-500'>
+                <Text className='text-green-500'>{rating}</Text> . rating
+              </Text>
+            </View>
+
+            <View className='flex-row items-center space-x-1'>
+              <MapPinIcon color='gray' opacity={0.4} size={22} />
+              <Text className='text-xs text-gray-500'>Nearby . {address}</Text>
+            </View>
+          </View>
+
+          <Text className='text-gray-500 mt-2 pb-4'>{short_description}</Text>
+        </View>
+
+        <TouchableOpacity className='flex-row items-center space-x-2 p-4 border-y border-gray-300'>
+          <QuestionMarkCircleIcon color='gray' opacity={0.6} size={20} />
+          <Text className='pl-2 flex-1 text-md font-bold'>
+            Have a food allergy?
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text className='px-4 pt-6 mb-3 font-bold text-xl'>Menu</Text>
+      </View>
+
+      {/* DishRows */}
+      {dishes.map((dish) => (
+        <DishRow
+          key={dish._id}
+          id={dish._id}
+          name={dish.name}
+          description={dish.short_description}
+          price={dish.price}
+          image={dish.image}
+        />
+      ))}
     </ScrollView>
   );
 };
