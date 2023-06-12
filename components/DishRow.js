86e996ae2f3f6a5f46b7c4a4313c02bebd440468
volkovich <volkoviysash@gmail.com>
2023-06-12 17:27:10 +0300

    feat: Create basket component.

--- components/DishRow.js
+++ components/DishRow.js
@@ -24,6 +24,7 @@ const DishRow = ({ id, name, description, price, image }) => {
 
   const removeItemFromBasket = () => {
     if (!items.length > 0) return;
+
     dispatch(removeFromBasket({ id }));
   };
 
@@ -57,18 +58,13 @@ const DishRow = ({ id, name, description, price, image }) => {
           <View className='flex-row items-center space-x-2 pb-3'>
             <TouchableOpacity onPress={removeItemFromBasket}>
               <MinusCircleIcon
-                // color={items.length > 0 ? "#00CCBB" : "gray"}
-                color='#00CCBB'
+                color={items.length > 0 ? "#00CCBB" : "gray"}
                 size={40}
               />
             </TouchableOpacity>
             <Text>{items.length}</Text>
             <TouchableOpacity onPress={addItemToBasket}>
-              <PlusCircleIcon
-                // color={items.length > 0 ? "#00CCBB" : "gray"}
-                color='#00CCBB'
-                size={40}
-              />
+              <PlusCircleIcon color='#00CCBB' size={40} />
             </TouchableOpacity>
           </View>
         </View>
