6c9074aec312dcddfa3d7fa501bff52b483dad75
volkovich <volkoviysash@gmail.com>
2023-06-14 02:26:09 +0300

    modify: Change loading element in the loading page.

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,10 +1,19 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as Animatable from "react-native-animatable";
 import * as Progress from "react-native-progress";
+import { useNavigation } from "@react-navigation/native";
 
 const PreparingOrderScreen = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate("Delivery");
+    }, 4000);
+  }, []);
+
   return (
     <SafeAreaView className='bg-[#dad4e1] flex-1 justify-center items-center'>
       <Animatable.Image
@@ -20,7 +29,7 @@ const PreparingOrderScreen = () => {
       >
         Waiting for Restaurant to accept your order!
       </Animatable.Text>
-      <Progress.Bar indeterminate={true} color='white' />
+      <Progress.Circle size={30} indeterminate={true} color='white' />
     </SafeAreaView>
   );
 };
