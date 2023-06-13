8771d0c15c023c773865e31fc63bfcccd509b50a
volkovich <volkoviysash@gmail.com>
2023-06-13 22:12:11 +0300

    feat: Building basket screen.

---
+++ screens/BasketScreen.js
@@ -0,0 +1,21 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
+import { selectBasketItems } from "../features/basketSlice";
+import { selectRestaurant } from "../features/restaurantSlice";
+
+const BasketScreen = () => {
+  const navigation = useNavigation();
+  const restaurant = useSelector(selectRestaurant);
+  const items = useSelector(selectBasketItems);
+  const dispatch = useDispatch();
+
+  return (
+    <View>
+      <Text>BasketScreen</Text>
+    </View>
+  );
+};
+
+export default BasketScreen;
